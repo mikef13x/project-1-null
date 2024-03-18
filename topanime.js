f7ce62ebb21b31ae0a739821c01cf6684744c046
kaylommy <kaylajfreeman13@gmail.com>
2024-03-17 21:44:09 -0400

    added buttons and edited code so that each page will display next items

--- topanime.js
+++ topanime.js
@@ -3,21 +3,26 @@ var currentPage = 1;
 var itemsPerPage = 25;
 
 function getTopAnime(){
-fetch(topAnimeURL).then(response => response.json()).then(data => {
-    for (let i = 0; i < itemsPerPage; i++) {
-        var animeTitle = data.data[i].titles[0].title;
-        var synopsis = data.data[i].synopsis;
-        var rank = 'Rank: ' + data.data[i].rank;
-        var score = '⭐' + data.data[i].score;
-        var image = data.data[i].images.jpg.small_image_url;
+    fetch(`${topAnimeURL}?page=${currentPage}`)
+    .then(response => response.json())
+    .then(data => {
+        for (let i = 0; i < itemsPerPage; i++) {
+            var animeTitle = data.data[i].titles[0].title;
+            var synopsis = data.data[i].synopsis;
+            var rank = 'Rank: ' + data.data[i].rank;
+            var score = '⭐' + data.data[i].score;
+            var image = data.data[i].images.jpg.small_image_url;
+            var episodes = '(' + data.data[i].episodes + ' eps)';
 
     var animeCard = document.createElement('div');
     animeCard.classList.add('box');
-
+// ADD BUTTON TO ADD ANIME TO 'MY LIST'
     animeCard.innerHTML = `
-    <h2>${animeTitle}</h2>
     <img src="${image}">
+    <div class='anime-info'>
+    <h1><strong>${animeTitle}</strong></h1>
     <h4><strong>${rank}</strong></h4>
+    <p>${episodes}</p>
     <p>${synopsis}</p>
     <p>${score}</p>
     `;
@@ -27,11 +32,18 @@ var nextPageButton = document.createElement('button');
 nextPageButton.textContent = 'Next Page';
 nextPageButton.addEventListener('click', () => {
     currentPage++;
-    document.querySelector('.top-anime-content').innerHTML = ''; // Clear previous content
+    document.querySelector('.top-anime-content').innerHTML = '';
     getTopAnime();
 });
+var prevPageButton = document.createElement('button');
+prevPageButton.textContent = 'Previous Page';
+prevPageButton.addEventListener('click', () => {
+    currentPage--;
+    document.querySelector('.top-anime-content').innerHTML = '';
+    getTopAnime();
+})
+document.querySelector('.top-anime-content').appendChild(prevPageButton);
 document.querySelector('.top-anime-content').appendChild(nextPageButton);
-
 })
 }
 
