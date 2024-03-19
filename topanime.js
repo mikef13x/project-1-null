b914792425741a50ecd02553fedcc5deccad451d
mikef13x <mikef132044@gmail.com>
2024-03-18 20:59:16 -0400

    added addtoMyList Button and allowed title and image data to be saved into local storage

--- topanime.js
+++ topanime.js
@@ -1,23 +1,34 @@
 var topAnimeURL = 'https://api.jikan.moe/v4/top/anime';
 var currentPage = 1;
 var itemsPerPage = 25;
+var animeList = JSON.parse(localStorage.getItem("Anime")) || []
 
-function getTopAnime(){
+
+
+
+function getTopAnime() {
     fetch(`${topAnimeURL}?page=${currentPage}`)
-    .then(response => response.json())
-    .then(data => {
-        for (let i = 0; i < itemsPerPage; i++) {
-            var animeTitle = data.data[i].titles[0].title;
-            var synopsis = data.data[i].synopsis;
-            var rank = 'Rank: ' + data.data[i].rank;
-            var score = '⭐' + data.data[i].score;
-            var image = data.data[i].images.jpg.small_image_url;
-            var episodes = '(' + data.data[i].episodes + ' eps)';
+        .then(response => response.json())
+        .then(data => {
+            for (let i = 0; i < itemsPerPage; i++) {
+                var animeTitle = data.data[i].titles[0].title;
+                var synopsis = data.data[i].synopsis;
+                var rank = 'Rank: ' + data.data[i].rank;
+                var score = '⭐' + data.data[i].score;
+                var image = data.data[i].images.jpg.small_image_url;
+                var episodes = '(' + data.data[i].episodes + ' eps)';
+                var malId = data.data[i].mal_id;
+               
+                console.log(animeTitle)
 
-    var animeCard = document.createElement('div');
-    animeCard.classList.add('box');
-// ADD BUTTON TO ADD ANIME TO 'MY LIST'
-    animeCard.innerHTML = `
+
+                var animeCard = document.createElement('div');
+                animeCard.classList.add('box');
+
+
+
+                // ADD BUTTON TO ADD ANIME TO 'MY LIST'
+                animeCard.innerHTML = `
     <img src="${image}">
     <div class='anime-info'>
     <h1><strong>${animeTitle}</strong></h1>
@@ -25,26 +36,58 @@ function getTopAnime(){
     <p>${episodes}</p>
     <p>${synopsis}</p>
     <p>${score}</p>
+    <button id="${malId}" data-anime-title="${animeTitle.toString()}" data-anime-image=${image} class="button is-danger">Add to MyList</button>
     `;
-    document.querySelector('.top-anime-content').appendChild(animeCard);
-}
-var nextPageButton = document.createElement('button');
-nextPageButton.textContent = 'Next Page';
-nextPageButton.addEventListener('click', () => {
-    currentPage++;
-    document.querySelector('.top-anime-content').innerHTML = '';
-    getTopAnime();
-});
-var prevPageButton = document.createElement('button');
-prevPageButton.textContent = 'Previous Page';
-prevPageButton.addEventListener('click', () => {
-    currentPage--;
-    document.querySelector('.top-anime-content').innerHTML = '';
-    getTopAnime();
-})
-document.querySelector('.top-anime-content').appendChild(prevPageButton);
-document.querySelector('.top-anime-content').appendChild(nextPageButton);
-})
+              
+                document.querySelector('.top-anime-content').appendChild(animeCard);
+                var myListBtn = document.getElementById(malId);
+                myListBtn.addEventListener("click", setList)
+
+
+            }
+
+            var nextPageButton = document.createElement('button');
+            nextPageButton.textContent = 'Next Page';
+            nextPageButton.addEventListener('click', () => {
+                currentPage++;
+                document.querySelector('.top-anime-content').innerHTML = '';
+                getTopAnime();
+            });
+            var prevPageButton = document.createElement('button');
+            prevPageButton.textContent = 'Previous Page';
+            prevPageButton.addEventListener('click', () => {
+                currentPage--;
+                document.querySelector('.top-anime-content').innerHTML = '';
+                getTopAnime();
+            })
+            document.querySelector('.top-anime-content').appendChild(prevPageButton);
+            document.querySelector('.top-anime-content').appendChild(nextPageButton);
+        })
 }
 
 getTopAnime();
+
+
+function setList(event) {
+    console.log("working", event.target.getAttribute("data-anime-title"));
+    var animeTitle = event.target.getAttribute("data-anime-title");
+    console.log(animeTitle);
+    var imgUrl = event.target.getAttribute("data-anime-image");
+    console.log(imgUrl);
+    console.log(animeList);
+    var animeObj = {
+        title: animeTitle,
+        image: imgUrl,
+    }
+    console.log(animeObj);
+    animeList.push(animeObj);
+    console.log(animeList);
+    localStorage.setItem("Anime", JSON.stringify(animeList));
+    //     if (animeList.join("").includes(animeCard) === false) {
+    //         animeList.push(animeCard)
+    //         localStorage.setItem("Anime", JSON.stringify(animeList))
+
+    //     }
+
+}
+
